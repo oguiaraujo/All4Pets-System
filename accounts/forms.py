330dcd133f36350264cb11ded73ae1c8c72e8e0f
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-08 19:03:43 -0300

    feat(forms): Adiciona formulario de registro de usuario #23
    Este formulário é para registrar um novo usuário e pode ser acessado por usuários comuns.

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,4 +9,17 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
   class Meta:
     model = User
-    filds = ('email','first_name','last_name','cpf','telefone','role', 'isactive')
+    filds = ('email','first_name','last_name','cpf','telefone','role','isactive')
+
+class SignUpForm(UserCreationForm):
+  class Meta:
+    model = User
+    filds = ('email','first_name','last_name','cpf','telefone','password1','password2')
+    
+  def save(self, commit = True):
+    user = super().save(commit = False)
+    user_role = User.Roles.CLIENT
+    user.is_staff = False
+    if commit:
+      user.save()
+    return user 
