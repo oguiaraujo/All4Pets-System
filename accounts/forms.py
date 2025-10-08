bb7b4e473433b2f43d6411d60b8bf85e5737cbd3
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-08 14:28:25 -0300

    feat(forms): Adiciona formulario de alteracao de usuario #23

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
   class Meta:
     model = User
     filds = ('email','first_name','last_name','cpf','telefone','role')
+
+class CustomUserChangeForm(UserChangeForm):
+  class Meta:
+    model = User
+    filds = ('email','first_name','last_name','cpf','telefone','role', 'isactive')
