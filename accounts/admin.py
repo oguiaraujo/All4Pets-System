c5557a79663b5ededd61e5ba45c0531322ac225d
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-08 21:15:28 -0300

    feat(admin): cria classe para admin gerenciar usuarios #23

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+  add_form = CustomUserCreationForm
+  form = CustomUserChangeForm
+  model = User
+  list_display = ('email','first_name','role','is_staff','is_active')
+  list_filter = ('role','is_staff','is_active')
+  fieldsets = (
+      (None, {'fields': ('email','password')}),
+      ('Informações pessoais', {'fields': ('first_name','last_name','cpf','telefone','cargo')}),
+      ('Permissões', {'fields': ('is_active','is_staff','is_superuser','groups')}),
+      ('Datas', {'fields': ('last_login','date_joined')})
+  )
+  add_fieldsets = (
+      (None, {
+          'classes': ('wide',),
+          'fields': ('email','first_name','password1','password2','role','is_staff','is_active')
+      }),
+  )
+  search_fields = ('email','first_name','cpf')
+  ordering = ('email',)
+
+admin.site.register(User, UserAdmin)
