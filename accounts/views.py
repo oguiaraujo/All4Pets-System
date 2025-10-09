914e32b236c4585db02e232a1a54aee33631105a
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-09 09:28:22 -0300

    feat(views): adiciona view de logout #23

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 class CustomLoginView(LoginView):
   template_name = 'accounts/login.html'
+
+class CustomLogoutView(LogoutView):
+  next_page = reverse_lazy('login')
