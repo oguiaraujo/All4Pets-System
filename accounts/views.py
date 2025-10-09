3a869a9ca23fbb9acaf62bf5a741b42bb0595b41
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-09 09:41:02 -0300

    feat(views): adiciona view de signup #23
    O signup pode ser feito com métodos get e post.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,25 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.views import View
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
   template_name = 'accounts/login.html'
 
 class CustomLogoutView(LogoutView):
   next_page = reverse_lazy('login')
+
+class SignUpView(View):
+  def get(self, request):
+    form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form' : form})
+  
+  def post(self, request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    return render(request, 'accounts/signup.html', {'form' : form})
