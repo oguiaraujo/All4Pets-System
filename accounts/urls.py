95e0ffa2d7d9a06beb29b06f0cbace04e6fbb566
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-11 12:55:58 -0300

    fix(urls): corrige implementação de path #23

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CustomLoginView, CustomLogoutView, SignUpView
 
 urlpatterns = [
-    path('login/', CustomLoginView, name='login'),
-    path('logout/', CustomLogoutView, name='logout'),
-    path('signup/', SignUpView, name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
