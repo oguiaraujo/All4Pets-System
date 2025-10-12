d56b0ba572a98d6e9149cbc6b9ff6ad75b91fb44
Guilherme Araújo <161907293+oguiaraujo@users.noreply.github.com>
2025-10-12 15:17:33 -0300

    feat: implementa tela de login e autenticacao de usuarios (US08) (#28)
    * build: Cria o Core do Sistema #23
    
    Este commit inicializa a estrutura base do projeto Django para o sistema All4Pets.
    
    Principais alterações:
    - Criação do projeto e do app de configuração principal chamado `core`.
    - Configuração dos arquivos essenciais: `settings.py`, `urls.py`, `asgi.py`, e `wsgi.py`.
    - Geração do banco de dados inicial `db.sqlite3` para desenvolvimento local.
    
    Este é o ponto de partida para a construção de todas as funcionalidades futuras, estabelecendo a fundação do backend.
    
    * build: Cria App accounts #23
    
    * feat: Adiciona o Modelo de Usuarios
    
    * feat(settings): define custom user model #23
    
    * feat(forms): Adiciona formulario de criacao de usuario #23
    
    * feat(forms): Adiciona formulario de alteracao de usuario #23
    
    * feat(forms): Adiciona formulario de registro de usuario #23
    
    Este formulário é para registrar um novo usuário e pode ser acessado por usuários comuns.
    
    * feat(admin): cria classe para admin gerenciar usuarios #23
    
    * feat(views): adiciona view de login #23
    
    * feat(views): adiciona view de logout #23
    
    * feat(views): adiciona view de signup #23
    
    O signup pode ser feito com métodos get e post.
    
    * feat(templates): adiciona tela de login #23
    
    * feat(templates): adiciona tela de signup #23
    
    * feat(settings): adiciona redirecionamento de home e login #23
    
    * feat(settings): instala o app accounts #23
    
    * feat(urls): define rotas do app accounts #23
    
    * feat(urls): adiciona rotas do app accounts no core #23
    
    * chore(settings): altera LOGIN_REDIRECT_URL para 'login' #23
    
    * fix(forms): corrige erro de digitação em fields #23
    
    * fix(forms): corrige erro de digitação em 'is_active' #23
    
    * fix(admin): corrige registro de modelo admin #23 #23
    
    * fix(urls): corrige implementação de path #23
    
    * feat(base): cria template base do html #23
    
    * feat(settings): define o template base do html #23
    
    * fix(login): remove chamada a reset password #23
    
    * feat(migrations): faz migracao do app accounts #23
    
    * fix(views): corrige redirecionamento apos signup #23

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ('email','first_name','last_name','cpf','telefone','role')
+
+class CustomUserChangeForm(UserChangeForm):
+  class Meta:
+    model = User
+    fields = ('email','first_name','last_name','cpf','telefone','role','is_active')
+
+class SignUpForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ('email','first_name','last_name','cpf','telefone','password1','password2')
+    
+  def save(self, commit = True):
+    user = super().save(commit = False)
+    user_role = User.Roles.CLIENT
+    user.is_staff = False
+    if commit:
+      user.save()
+    return user 
