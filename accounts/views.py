3d13b54b02e17813ddec209584cc8a25295da13c
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2025-10-20 20:34:50 -0300

    feat(views): implementa views da api rest #34

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,10 @@ from django.views import View
 from .forms import SignUpForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
 
 class CustomLoginView(LoginView):
   template_name = 'accounts/login.html'
@@ -23,3 +27,14 @@ class SignUpView(View):
       login(request, user)
       return redirect('login')
     return render(request, 'accounts/signup.html', {'form' : form})
+  
+class UserViewSet(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+  def get_permissions(self):
+    if self.action in ['list', 'retrieve']:
+      permission_classes = [IsAuthenticated]
+    else:
+      permission_classes = [IsAdminUser]
+    return [permission() for permission in permission_classes]
