958e1e9345612d253537bc9c60e8f3afcca4b986
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-10-22 21:12:47 -0300

    feat: adiciona método __str__ na classe Service #33

--- services/models.py
+++ services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    duration = models.IntegerField()
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        if self.status==True:
+            status = "Ativo"
+        else:
+            status = "Inativo"
+        return f"{self.name} - {status}"
