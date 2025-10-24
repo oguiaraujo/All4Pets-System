fdc6013fc36b5e17fd6251f3b7e7cc08b9eb6c08
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-10-23 23:17:30 -0300

    Revert "feat: adiciona ServiceViewSet para o CRUD de servicos"
    This reverts commit 59cbbf98f459d7ce7093c90f95446976656a81c2.

--- services/views.py
+++ services/views.py
@@ -1,18 +1,3 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from .models import Service
-from .serializers import ServiceSerializer
+from django.shortcuts import render
 
 # Create your views here.
-class ServiceViewSet(viewsets.ModelViewSet):
-    queryset = Service.objects.all()
-    serializer_class = ServiceSerializer
-
-    def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
-            permission_classes = [IsAuthenticated]
-        else:
-            permission_classes = [IsAdminUser]
-        return [permission() for permission in permission_classes]
