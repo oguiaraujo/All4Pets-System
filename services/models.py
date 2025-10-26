f69cbaf357066229bfd3048d69b4fab36f7d8af1
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-10-26 13:31:09 -0300

    feat: torna campo descrição opcional no cadastro de serviços #33

--- services/models.py
+++ services/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=40)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     duration = models.IntegerField()
     status = models.BooleanField(default=True)
