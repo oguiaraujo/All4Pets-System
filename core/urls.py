691a2a206850f513382bfb885c392fc88e9173aa
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-10-26 17:50:45 -0300

    fix: corrige conflito de URLs entre API e templates #38

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from accounts.views import UserViewSet
+from services.views import ServiceViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register('services', ServiceViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path('api/', include(router.urls)),
-    path('api/services/', include('services.urls')),
-    path('services/', include('services.urls'))
+    path('services/', include('services.urls')),
+    path('api/', include(router.urls)),
 ]
